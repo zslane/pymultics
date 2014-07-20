12cd7ab66d960180ee7bbad60706612de91c5504
John Cooper <john@jrcooper.com>
2014-07-19 23:24:40 -0700

    Fixes to ioa_

--- multics/filesystem/sss/ioa_.py
+++ multics/filesystem/sss/ioa_.py
@@ -5,12 +5,24 @@ from multics.globals import *
 def _flatten(l):
     r = []
     for x in l:
-        if hasattr(x, '__iter__'):
-            y = _flatten(x)
-            r.extend(y)
+        if type(x) is list:
+            r.extend(_flatten(x))
         else:
             r.append(x)
+        # end if
+    # end for
     return r
+    
+def _parse_number(clist):
+    #== Return None if there is no leading digit to parse
+    if not (clist and clist[0].isdigit()):
+        return None
+    # end if
+    n = 0
+    while clist and clist[0].isdigit():
+        n = n * 10 + int(clist.pop(0))
+    # end while
+    return n
     
 def _edit_string_token(s, N):
     return "%-*s" % (N or 0, s)
@@ -53,12 +65,15 @@ def _edit_bitstring_token(b, N, D):
 def _ignore_args(args, N):
     if N is None:
         N = 1
+    # end if
     for i in range(N):
         try:
             x = args.pop(0)
             #print "IGNORE:", repr(x)
         except:
             pass
+        # end try
+    # end for
     return ""
 
 def _edit_tabpos_token(string_so_far, N, D):
@@ -67,35 +82,43 @@ def _edit_tabpos_token(string_so_far, N, D):
     return " " * ((N or 10) - len(s) - 1)
     
 def _scan_case_block(clist):
+    #== Scan the block-start token '^['
     s  = clist.pop(0)
     s += clist.pop(0)
-    while len(clist) > 1 and clist[:2] != ["^","]"]:
+    while len(clist) >= 2 and clist[:2] != ["^","]"]:
         if clist[:2] == ["^","["]:
             s += _scan_case_block(clist)
         else:
             s += clist.pop(0)
+        # end if
+    # end while
+    #== Scan the block-end token '^]'
     s += clist.pop(0)
     s += clist.pop(0)
     return s
     
 def _scan_case_token(clist):
     s = ""
-    while len(clist) > 1 and clist[:2] != ["^",";"] and clist[:2] != ["^","]"]:
+    while len(clist) >= 2 and clist[:2] != ["^",";"] and clist[:2] != ["^","]"]:
         if clist[:2] == ["^","["]:
             s += _scan_case_block(clist)
         else:
             s += clist.pop(0)
+        # end if
+    # end while
     clist.pop(0)
     if clist[0] == ";":
         clist.pop(0)
+    # end if
     return s
 
 def _parse_case_token(clist, case_index, args):
     cases = []
-    while len(clist) > 1 and clist[0] != "]":
+    while clist and clist[0] != "]":
         s = _scan_case_token(clist)
         #print "CASE:", repr(s)
         cases.append(s)
+    # end while
     clist.pop(0)
     #print "Cases:", cases, "***", case_index
     if case_index is True:
@@ -104,12 +127,16 @@ def _parse_case_token(clist, case_index, args):
         case_index = 1
     else:
         case_index = int(case_index)
+    # end if
     try:
         for t in reversed(list(cases[case_index])):
             clist.insert(0, t)
-        return _begin_parse(clist, args)
+        # end for
+        # return _parse_begin(clist, args)
     except:
-        return ""
+        # return ""
+        pass
+    return ""
 
 def _parse_iter_token(clist, args, N):
     s = ""
@@ -121,29 +148,32 @@ def _parse_iter_token(clist, args, N):
             s += _parse_begin(clist2, args)
             if len(args) == len_before:
                 break
+            # end if
+        # end while
         clist[:] = clist2
     elif N:
         for i in range(N):
             clist2 = clist[:]
             s += _parse_begin(clist2, args)
+        # end for
         clist[:] = clist2
     else:
         _parse_begin(clist, args)
+    # end if
     return s
-    
+
 _REPEATABLE_CHARS = {'/':"\n", '-':"\t", '|':chr(12), 'x':" ", '^':"^"}
-    
+
 def _parse_edit_token(string_so_far, clist, args, N=None, D=None):
 
     c = clist[0]
     
     if c.isdigit() or c == ".":
-        while clist and clist[0].isdigit():
-            N = (N or 0) * 10 + int(clist.pop(0))
+        N = _parse_number(clist)
         if clist and clist[0] == ".":
             clist.pop(0)
-            while clist and clist[0].isdigit():
-                D = (D or 0) * 10 + int(clist.pop(0))
+            D = _parse_number(clist)
+        # end if
         return _parse_edit_token(string_so_far, clist, args, N, D)
         
     elif len(clist) > 2 and clist[:3] == "v.v":
@@ -157,12 +187,15 @@ def _parse_edit_token(string_so_far, clist, args, N=None, D=None):
             N = int(args.pop(0))
         else:
             D = int(args.pop(0))
+        # end if
         clist.pop(0)
         return _parse_edit_token(string_so_far, clist, args, N, D)
         
     elif c in _REPEATABLE_CHARS:
         clist.pop(0)
-        if N == 0: return ""
+        if N == 0:
+            return ""
+        # end if
         return _REPEATABLE_CHARS[c] * (N or 1)
         
     elif c == "a":
@@ -217,6 +250,7 @@ def _parse_edit_token(string_so_far, clist, args, N=None, D=None):
     # elif c == ";":
         # print "NEXT"
         # return ""
+        
     elif c == "(":
         clist.pop(0)
         return _parse_iter_token(clist, args, N)
@@ -240,12 +274,15 @@ def _parse_begin(clist, args):
             elif clist and clist[0] == ")":
                 clist.pop(0)
                 return s
+            # end if
             
             x = _parse_edit_token(s, clist, args)
             #print "TOK:", repr(x), "->", repr(s+x)
             s += x
         else:
             s += clist.pop(0)
+        # end if
+    # end while
     return s
     
 def _ioa_parser(fmt, args):
