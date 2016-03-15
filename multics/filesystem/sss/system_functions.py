0feee6503dfcb7b6646cdd8fe0837db6b5c28f86
John Cooper <johnrcooper@gmail.com>
2016-03-14 20:24:25 -0700

    Added commented out code to unique function showing original Multics algorithm for generating shrieknames.

--- multics/filesystem/sss/system_functions.py
+++ multics/filesystem/sss/system_functions.py
@@ -37,6 +37,16 @@ def resolve_path_symbol_(path_symbol, frame_id=None):
 __xlate_table = string.maketrans(string.hexdigits[:16], "BWrtxJNmwpHZbLqz")
 
 def unique_name_(value, xlate_table=[]):
+    #== Below is the actual Multics algorithm:
+    #==   Grab the value 5 bits at a time and use each 5-bit value as
+    #==   an index into the table of characters. The string of chars
+    #==   assigned to table comes straight from unique_chars_.pl1
+    # table = "BCDFGHJKLMNPQWXZbcdfghjklmnpqwxz"
+    # c = ["!"]
+    # b = "{0:036b}".format(value)[-36:]
+    # for i in range(0, len(b), 5):
+        # c.append(table[int(b[i:i+5], 2)])
+    # return "".join(c)
     return "!" + "{0:09x}".format(value)[-9:].translate(__xlate_table)
 
 def active_function():
